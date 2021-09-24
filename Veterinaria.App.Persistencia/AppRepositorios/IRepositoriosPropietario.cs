7a0296ca0151402ea6c45ed648d34275a97842a7
ingemateo30 <msalazar5@udi.edu.co>
2021-09-24 10:56:01 -0500

    aplicacion de irepositorio propietario

---
+++ Veterinaria.App.Persistencia/AppRepositorios/IRepositoriosPropietario.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Veterinaria.App.Dominio;
+
+namespace Veterinaria.App.Persistencia
+{
+    public interface IRepositorioPropietario
+    {
+        Propietario AddPropietario(Propietario propietario);
+        void DeletePropietario(int idPropietario);
+        IEnumerable<Propietario> GetAllPropietario();
+        Propietario GetPropietario(int idPropietario);
+        Propietario UpdatePropietario(Propietario propietario);
+        Propietario UpdatePropietario(Propietario propietario, int idPropietario);
+    }
+
+
+}
---
+++ Veterinaria.App.Persistencia/AppRepositorios/RepositorioPropietario.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System.Linq;
+using Veterinaria.App.Dominio;
+
+namespace Veterinaria.App.Persistencia
+{
+    public class RepositorioPropietario : IRepositorioPropietario
+    {
+
+    }
+}
