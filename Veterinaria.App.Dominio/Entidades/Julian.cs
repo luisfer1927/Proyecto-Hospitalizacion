76c414311abd962a40f5a050d89f306757a1716b
julavil127 <julavil127@gmail.com>
2021-09-28 10:32:25 -0500

    cambio hecho por julian

---
+++ Veterinaria.App.Dominio/Entidades/Julian.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Veterinaria.App.Dominio
+{
+  public class Persona
+  {
+      public string Nombre{get;set;}
+      public string Apellidos{get;set;}
+      public string Telefono{get;set;}
+      public int Id {get;set;}
+  }
+}
