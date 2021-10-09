eadd7d7d624834caf4443633a944748ec59328f4
ingemateo30 <msalazar5@udi.edu.co>
2021-10-09 12:33:08 -0500

    estado de salud

--- Veterinaria.App.Persistencia/AppRepositorios/RepositorioEstadosalud.cs
+++ Veterinaria.App.Persistencia/AppRepositorios/RepositorioEstadosalud.cs
@@ -14,9 +14,9 @@ namespace Veterinaria.App.Persistencia.AppRepositorios
         {
             salud = new List<EstadodeSalud>()
             {
-                new EstadodeSalud{id=1,temperatura=33,Frecu_Respi=33,Frecu_Cardia=33,Estado_Animo="desanimado",recomendacion="mejorar alimentacion",Peso=12,masco_estadosalud=new Mascota{id=5,Nombre="TROSKI",Edad="15",Tipo_Mascota="PERRO",Estado_Salud="SALUDABLE",dueño="MATEO"},visit=new Visita{id=1,Fecha="12-07-18",Motivo_Visita="estado salud",veterinario_visita=new Veterinario{Id=1,Nombre="Mateo",Apellidos="Salazar Ortiz",Telefono="3182909852",Targeta_Profesional=123}}},
-                
-
+                new EstadodeSalud{id=1,temperatura=33,Frecu_Respi=33,Frecu_Cardia=33,Estado_Animo="desanimado",recomendacion="mejorar alimentacion",Peso=12,masco_estadosalud=new Mascota{id=1,Nombre="TROSKI",Edad="15",Tipo_Mascota="PERRO",Estado_Salud="SALUDABLE",dueño="MATEO"},visit=new Visita{id=1,Fecha="12-07-18",Motivo_Visita="estado salud",veterinario_visita=new Veterinario{Id=1,Nombre="Mateo",Apellidos="Salazar Ortiz",Telefono="3182909852",Targeta_Profesional=123}}},
+                new EstadodeSalud{id=2,temperatura=33,Frecu_Respi=33,Frecu_Cardia=33,Estado_Animo="Feliz",recomendacion="segguir asi",Peso=7,masco_estadosalud=new Mascota{id=2,Nombre="Firulais",Edad="15",Tipo_Mascota="PERRO",Estado_Salud="SALUDABLE",dueño="MATEO"},visit=new Visita{id=1,Fecha="12-08-18",Motivo_Visita="estado salud",veterinario_visita=new Veterinario{Id=2,Nombre="Mateo",Apellidos="Salazar Ortiz",Telefono="3182909852",Targeta_Profesional=123}}},
+                new EstadodeSalud{id=3,temperatura=33,Frecu_Respi=33,Frecu_Cardia=33,Estado_Animo="bien",recomendacion="limpiar la boca seguido",Peso=11,masco_estadosalud=new Mascota{id=3,Nombre="luna",Edad="15",Tipo_Mascota="PERRO",Estado_Salud="SALUDABLE",dueño="MATEO"},visit=new Visita{id=1,Fecha="12-010-18",Motivo_Visita="estado salud",veterinario_visita=new Veterinario{Id=3,Nombre="Mateo",Apellidos="Salazar Ortiz",Telefono="3182909852",Targeta_Profesional=123}}},
             };
         }
         public EstadodeSalud Addsalud(EstadodeSalud salud)
